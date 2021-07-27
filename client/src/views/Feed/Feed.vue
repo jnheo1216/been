fa3580dff31442a4c7204b8cb8295a3d0692ee3b
Hwajeong Lee <honeylee999@icloud.com>
2021-07-27 16:21:59 +0900

    [FE]0727_client/src/views/Feed.vue - 빌드

--- client/src/views/Feed/Feed.vue
+++ client/src/views/Feed/Feed.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
+  <div class="mainfeed">
+    <div class="logo">
+      <img alt="BEEN LOGO" src="/assets/logo.png">
+    </div>
+
+    <div class="hex">
+      <ul class="infinite-list" v-infinite-scroll="load" style="overflow:auto">
+        <li class="infinite-list-item">{{ i }}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'Feed',
-}
+  export default {
+    name: 'Feed',
+    data () {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+      load () {
+        this.count += 2
+      }
+    }
+  }
 </script>
 
 <style>
+  .mainfeed {
+    width: 375px;
+    height: 812px;
+    background-color: #ffe8c4
+  }
 
+  .hex {
+    width: 25%;
+    position: relative;
+  }
 </style>
