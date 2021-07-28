7be70dfc6b8165659c783b7e8f5f8283ce0f389c
Hwajeong Lee <honeylee999@icloud.com>
2021-07-28 09:10:07 +0900

    [FE]0728_client/src/components/Feed,Search 빌드

---
+++ client/src/components/Search/TypeSearch.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="searchMain">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchMain',
+}
+</script>
+
+<style>
+  .searchMain {
+    width: 375px;
+    height: 812px;
+    background-color: #fffaf4;
+    margin: 0 auto;
+  }
+</style>
--- client/src/views/Search/Search.vue
+++ client/src/views/Search/Search.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
+  <div class="Search">
+    <button @keypress="toTypeSearch">
+    <TypeSearch />
   </div>
 </template>
 
 <script>
+import TypeSearch from '@/components/Search/TypeSearch.vue'
 
 export default {
   name: 'Search',
+  components: {
+    TypeSearch,
+  },
+  methods: {
+    toTypeSearch: function () {
+      
+    }
+  }
 }
 </script>
 
