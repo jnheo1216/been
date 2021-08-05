91c927363a604270912e6cba3a2cda49569898eb
jnheo1216 <hjeionyng94@gmail.com>
2021-08-05 16:56:30 +0900

    [FE]0805_src/store 로그인 유저 상태 저장

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,40 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
+  state: {    
+    isLogin: false,
+    user: {
+      id: "",
+      email: "",
+      password: "",
+      intro: "",
+      nickname: "",
+      name: "",
+      tier: 0,
+      followerCnt: 0,
+      followingCnt: 0,
+      createdAt: "",
+      profilePicSrc: "",
+      profilePicName: "",
+      emailConfirmation: 0,
+      status: ''
+    },
   },
   mutations: {
+    setUserInfo(state, data) {
+      state.user = data;
+      state.isLogin = true;
+    },
+    logout(state) {
+      state.isLogin = false;
+      state.user=null;
+    },
   },
   actions: {
+    LOGOUT({commit}) {
+      commit('logout');
+      localStorage.removeItem("access-token");
+    },
   },
   modules: {
   }
