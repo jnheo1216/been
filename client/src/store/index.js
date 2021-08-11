27e31202388b97424e27a19bf99db02b8850314c
alpaka99 <ruvan123@naver.com>
2021-08-11 16:05:29 +0900

    [FE]0811/index.js 포스트 등록 내용 넣음

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,7 +4,18 @@ import router from '../router'
 export default createStore({
   state: {
     user: {},
-    jwtAuthToken: localStorage.getItem('jwt-auth-token') || ''
+    jwtAuthToken: localStorage.getItem('jwt-auth-token') || '',
+    post: {
+      content: '',
+      createdAt: '',
+      likeCnt: 0,
+      postId: 0,
+      postPicName: '',
+      postPicSrc: '',
+      title: '',
+      updatedAt: '',
+      userId: ''
+    }
   },
   getters: {
     isLoggedIn({ jwtAuthToken }) {
